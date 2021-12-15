8f75fb3ec6c3445496bc9e378a8570d1fc32b80c
gillianlentini <55030211+gillianlentini@users.noreply.github.com>
2021-12-15 03:09:35 -0500

    Update deep_q_learning.py

--- deep_q_learning.py
+++ deep_q_learning.py
@@ -1,24 +1,29 @@
+'''
+   This code was written by following the following PyTorch tutorial
+   Tutorial Title: TRAIN A MARIO-PLAYING RL AGENT
+   Project Title: MadMario
+   Author: Yuansong Feng, Suraj Subramanian, Howard Wang, Steven Guo
+   Date: June 2020
+   Code version: 2.0
+   Availability: https://pytorch.org/tutorials/intermediate/mario_rl_tutorial.html#train-a-mario-playing-rl-agent
+                 https://github.com/YuansongFeng/MadMario
+'''
+
 import torch
 from pathlib import Path
 import random, datetime, numpy as np, cv2
 from gym.wrappers import FrameStack, GrayScaleObservation, TransformObservation
 from agents.deep_q_learning_agent import DeepQLearningMario
 
-#NES Emulator for OpenAI Gym
 from nes_py.wrappers import JoypadSpace
 
-# Super Mario environment for OpenAI Gym
 import gym_super_mario_bros
 
-# Initialize Super Mario environment
 from util.deep_q_logging import MetricLogger
 from util.wrappers import SkipFrame, ResizeObservation
 
 env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0')
 
-# Limit the action-space to
-#   0. walk right
-#   1. jump right
 env = JoypadSpace(
     env,
     [['right'],
@@ -30,10 +35,6 @@ env = ResizeObservation(env, shape=84)
 env = TransformObservation(env, f=lambda x: x / 255.)
 env = FrameStack(env, num_stack=4)
 
-use_cuda = torch.cuda.is_available()
-print(f"Using CUDA: {use_cuda}")
-print()
-
 save_dir = Path('./data/checkpoints')
 
 mario = DeepQLearningMario(state_dim=(4, 84, 84), action_dim=env.action_space.n, save_dir=save_dir)
@@ -42,7 +43,6 @@ logger = MetricLogger(save_dir)
 
 episodes = 40000
 
-### for Loop that train the model num_episodes times by playing the game
 for e in range(episodes):
 
     state = env.reset()
@@ -50,25 +50,18 @@ for e in range(episodes):
     # Play the game!
     while True:
 
-        # Run agent on the state
         action = mario.act(state)
 
-        # Agent performs action
         next_state, reward, done, info = env.step(action)
 
-        # Remember
         mario.cache(state, next_state, action, reward, done)
 
-        # Learn
         q, loss = mario.learn()
 
-        # Logging
         logger.log_step(reward, loss, q)
 
-        # Update state
         state = next_state
 
-        # Check if end of game
         if done or info['flag_get']:
             break
 
